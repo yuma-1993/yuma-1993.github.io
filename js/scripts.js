96e773af21d4717f625166b6e6b5c1a159b7b4e9
Johaniel <johanielllopis@gmail.com>
2022-09-29 19:27:01 +0200

    Nueva versión movil 0.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
+//Guardamos en estas dos variables los elementos con esos ID
+const toggleMenuElement = document.getElementById('toggle-menu'); //Icono hamburguesa
+const mainMenuElement = document.getElementById('main-menu'); //Menu
 
+//Cuando se haga click, a ese mainMenuElement se le intercambiará la clase
+toggleMenuElement.addEventListener('click', ()=>{
+mainMenuElement.classList.toggle('main-menu--show') //Si tiene la clase se la quitamos y si no se la ponemos;
+});
